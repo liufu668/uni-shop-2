3d25edde7a209a6bf4001987423d4dddb26f3a2e
liufu668 <3328518414@qq.com>
2024-11-26 17:42:25 +0800

    完成了 home 首页的开发

---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    Goods_list
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
